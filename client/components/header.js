ac81713f463384dc9958e4b957079efbd15f0d02
Alexander Heo <jikster712@gmail.com>
2021-12-03 15:00:17 -0800

    feature: display average grade in header

--- client/components/header.js
+++ client/components/header.js
@@ -6,14 +6,12 @@ const header = ({ avgGrade }) => {
       <div className='heading'>
         <h1>Gradebook</h1>
       </div>
-      <div className='averageGradeContainer'>
-        <div className='averageGrade'>
-          <div className='averageGradeLabel'>
-            <h2>Average Grade:</h2>
-          </div>
-          <div className='averageGradeBox'>
-            <h2>{avgGrade}</h2>
-          </div>
+      <div className='averageGrade'>
+        <div className='averageGradeLabel'>
+          <h2>Average Grade:</h2>
+        </div>
+        <div className='averageGradeBox'>
+          <h2>{avgGrade}</h2>
         </div>
       </div>
     </div>
