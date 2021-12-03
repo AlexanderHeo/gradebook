6945dfb787bd21f890ee68e63f074f83c771da1c
Alexander Heo <jikster712@gmail.com>
2021-12-03 11:20:07 -0800

    fetch all grades

--- client/components/app.js
+++ client/components/app.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  state = {
+    grades: [],
+  };
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades = () => {
+    fetch('/api/grades')
+      .then((response) => response.json())
+      .then((jsonData) => {
+        this.setState({
+          grades: jsonData,
+        });
+      });
+  };
+
   render() {
     return <h1>TESTING TESTING</h1>;
   }
