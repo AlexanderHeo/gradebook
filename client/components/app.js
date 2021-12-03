7f14233e3cb91c27a1c0f50751dae3b6a17afc8b
Alexander Heo <jikster712@gmail.com>
2021-12-03 15:18:59 -0800

    fetch - add new student, recalculate average grade

--- client/components/app.js
+++ client/components/app.js
@@ -40,6 +40,21 @@ export default class App extends Component {
     });
   };
 
+  addNewGrade = (newStudent) => {
+    fetch('/api/grades', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newStudent),
+    })
+      .then((response) => response.json())
+      .then((jsonData) => {
+        const gradesCopy = [...this.state.grades];
+        const addedNewStudent = gradesCopy.concat(jsonData);
+        this.getAverageGrades(addedNewStudent);
+        this.setState({ grades: addedNewStudent });
+      });
+  };
+
   render() {
     return (
       <div className='wrapper'>
